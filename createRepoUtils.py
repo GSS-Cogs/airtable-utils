484a1a9d8463ced146b0c2a6b293e914f5823c05
LPerryman <leigh.perryman@gsscogs.uk>
2020-05-22 08:32:20 +0100

    pushing changes to my airtable script

--- createRepoUtils.py
+++ createRepoUtils.py
@@ -5,8 +5,12 @@ from git import Repo
 import json
 import re
 from gssutils import *
-import pandas as pd
-import secrets as ss
+
+
+REPOSYNC_CONFIG = Path.home() / '.config' / 'reposync'
+AIRTABLE_TOKEN_FILE = REPOSYNC_CONFIG / 'airtable-token'
+GITHUB_TOKEN_FILE = REPOSYNC_CONFIG / 'github-token'
+#JENKINS_TOKEN_FILE = REPOSYNC_CONFIG / 'jenkins-token'
 
 
 def createInfoJSONFile(pth, jsonInfo, inx, fleNme, gitNme):
@@ -29,15 +33,14 @@ def createInfoJSONFile(pth, jsonInfo, inx, fleNme, gitNme):
 
 
 # Check if the Family is the one you want, passed param has been changed for testing purposes
-def checkFamily(transformFam, wantedFam, airTableFamilies):
+def checkFamily(transformFam, airTableFamilies, wantedFam):
     try:
         # ASSUMES THEIR IS ONLY ONE FAMILY ASSOCIATED WITH THIS ETL
         # AND PULLS IN THE FIRST ONE FROM THE LIST (SHOULD ONLY BE ONE THING IN THE LIST)
         # --------------------------------------------------------------------------------------
         # --------------------------------------------------------------------------------------
         # THIS NEEDS TO BE REMOVED ONCE WORKING PROPERLY
-        wantedFam = 'Homelessness'
-        #wantedFam = 'Homelessness'
+        #wantedFam = 'COVID-19'
         # --------------------------------------------------------------------------------------
         # --------------------------------------------------------------------------------------
         ret = False
@@ -51,20 +54,58 @@ def checkFamily(transformFam, wantedFam, airTableFamilies):
         return False
 
 
-def seeIfFolderExists(tfNme, stage, dsPth, reportName):
+def seeIfFolderExists(tfNme, stage, dsPth):
     try:
-        pth = dsPth + '/' + tfNme
+        print("CHECKING IF FOLDER EXISTS: " + tfNme + " - " + dsPth)
+        # This is the current stage of the ETL
+        try:
+            pth = dsPth + '/' + str(stage[0]) + '/' + str(stage[1]) + '/' + tfNme
+        except Exception as e:
+            pth = dsPth + '/' + str(stage[0]) + '/' + tfNme
         pth = Path(pth)
 
-        if not pth.exists():
-            with open(reportName, "a") as repFle:
-                repFle.write('git add ' + tfNme + '\n')
+        pathExists = False
+        pthCheck = pth
+        fldrPaths = ['Hold', 'No Stage', 'To Do', 'To Do/Priority']
+        for fs in fldrPaths:
+            pthCheck = Path(dsPth + '/' + fs + '/' + tfNme)
+            if pthCheck.exists():
+                pathExists = True
+                break
 
-        pth.mkdir(exist_ok=True, parents=True)
+        if not pathExists:
+        #if not pth.exists():
+            # Folder DOES NOT exist so create it
+            pth.mkdir(exist_ok=True, parents=True)
+            print('Folder created: ' + str(pth))
+            return pth
+        else:
+            # Folder exists so check if it has been moved to a different stage
+            if pth != pthCheck:
+                print('\tSTAGE has changed for ETL ::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+                print('\t\tFrom: ' + str(pthCheck))
+                print('\t\tTo:' + str(pth))
+                shutil.move(pthCheck, pth)
+                print('\tETL Moved ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
 
-        return pth
+            return pthCheck
     except Exception as e:
         return 'seeIfFolderExists FAILURE: ' + str(e)
+
+# def seeIfFolderExists(tfNme, stage, dsPth, reportName):
+#    try:
+#        pth = dsPth + '/' + tfNme
+#        pth = Path(pth)
+
+#        if not pth.exists():
+#            with open(reportName, "a") as repFle:
+#                repFle.write('git add ' + tfNme + '\n')
+
+#        pth.mkdir(exist_ok=True, parents=True)
+
+#        return pth
+#    except Exception as e:
+#        return 'seeIfFolderExists FAILURE: ' + str(e)
 
 
 def getDataType(datTpe, tpeCde):
@@ -136,7 +177,7 @@ def createMainPYFile(pth, fNme, pyNme, jsNme):
         ret += 'from gssutils import * \n'
         ret += 'import json \n\n'
         ret += f'''info = json.load(open('{jsNme}')) \n'''
-        ret += f'''landingPage = info['Landing Page'] \n'''
+        ret += f'''landingPage = info['landingPage'] \n'''
         ret += 'landingPage \n\n'
         ret += '# + \n'
         ret += f'#### Add transformation script here #### \n\n'
@@ -150,35 +191,95 @@ def createMainPYFile(pth, fNme, pyNme, jsNme):
         return f'{pyNme} file creation failure: ' + str(e)
 
 
+def createttlmdtemplates(pth, fNme, ttlNme, mdNme, fam, jsonInfo, flrNme, user):
+    try:
+        fam = fam.replace('-AIRTABLE','')
+        mdPath = Path(pth / Path(mdNme))
+        #print('Creating MARKDOWN template:\n' + str(mdPath))
+        try:
+            lp = jsonInfo['fields']['Landing Page']
+        except Exception as e:
+            lp = 'PASTE LANDING PAGE HERE'
+            print('JSON: ' + str(e))
+
+        ret = '# COGS Dataset Specification\n\n'
+        ret += f'[Family Home](https://gss-cogs.github.io/{fam}/datasets/specmenu.html)\n\n'
+        ret += f'[Family Transform Status](https://gss-cogs.github.io/{fam}/datasets/index.html)\n\n'
+        ret += f'## {fNme.strip()} \n\n'
+        ret += f'[Landing Page]({lp})\n\n'
+        ret += f'[Transform Flowchart](https://gss-cogs.github.io/{fam}/datasets/specflowcharts.html?{flrNme}/flowchart.ttl)\n\n'
+        ret += '### Dataset One\n\n'
+        ret += '#### Output Dataset Name:\n\n' + '\t\tname\n\n'
+        ret += '#### Table Structure\n\n' + '\t\tPeriod, Measure Type, Unit, Marker, Value\n\n'
+        ret += '#### Sheet: 1\n\n' + '\t\tspec\n\n'
+        ret += '#### Sheet: 2\n\n' + '\t\tspec\n\n'
+        ret += '##### Footnotes\n\n' + '\t\tfootnotes\n\n'
+        ret += '##### DM Notes\n\n' + '\t\tnotes\n\n'
+
+        with open(mdPath, "w") as output:
+            output.write(ret)
+            output.close
+    except Exception as e:
+        print('MARKDOWN file creation failure: ' + str(e))
+
+    try:
+        ttlPath = Path(pth / Path(ttlNme))
+        #print('Creating TURTLE template:\n' + str(ttlPath))
+        f = open(f"//Users/{user}/Development/flowcharttemplate.ttl", "r")
+        with open(ttlPath, "w") as output:
+            output.write(f.read())
+            output.close
+    except Exception as e:
+        print('TURTLE creation failure: ' + str(e))
+
+
+
+
 def makeGithubIssue(title, family, stage, user):
     try:
-        otp = gitHubDetails(user)
-        g = Github(otp)
-        for repo in g.get_user().get_repos():
-            if repo.name == family:
-                myRepo = repo
-                break
-        label = myRepo.get_label(stage)
-        issue = myRepo.create_issue(title=title, body="Dataset Transformation/Pipeline creation", labels=[label])
-        issNum = issue.number
-        return issNum
+        if GITHUB_TOKEN_FILE.exists():
+            with open(GITHUB_TOKEN_FILE) as tf:
+                otp = tf.readline().rstrip('\n')
+
+            g = Github(otp)
+            for repo in g.get_user().get_repos():
+                if repo.name == family:
+                    myRepo = repo
+                    break
+            #label = myRepo.get_label(stage)
+            #issue = myRepo.create_issue(title=title, body="Dataset Transformation/Pipeline creation", labels=[label])
+            issue = myRepo.create_issue(title=title, body="Dataset Transformation/Pipeline creation")
+            issNum = issue.number
+            return issNum
+        else:
+            print('No TOKEN available to create issue')
+            return -1
     except Exception as e:
+        print('Error creating issue:' + str(e))
         return -1
 
 
 def checkIfIssueAlreadyExists(family, issueName, user):
     try:
-        otp = gitHubDetails(user)
-        g = Github(otp)
-        ret = -100
-        repo = g.get_repo('GSS-Cogs/' + family)
-        open_issues = repo.get_issues(state='open')
-        for issue in open_issues:
-            if issue.title == issueName:
-                ret = issue.number
-                break
-        return ret
+        if GITHUB_TOKEN_FILE.exists():
+            with open(GITHUB_TOKEN_FILE) as tf:
+                otp = tf.readline().rstrip('\n')
+
+            g = Github(otp)
+            ret = -100
+            repo = g.get_repo('GSS-Cogs/' + family)
+            open_issues = repo.get_issues(state='open')
+            for issue in open_issues:
+                print(issue.title + ' - ' + issueName)
+                if issue.title == issueName:
+                    ret = issue.number
+                    break
+            return ret
+        else:
+            print('No TOKEN available to check if issue exists')
+            return -100
     except Exception as e:
+        print('Error checking issue:' + str(e))
         return -100
 
 
@@ -233,41 +334,6 @@ def addCommitPushtoGit(fam):
     #except Exception as e:
         #print('Failed when adding-commiting-pushing to git: ' + str(e))
 
-
-# def seeIfFolderExists(tfNme, stage, dsPth):
-#    try:
-#        stage = stripString(stage)
-#        # This is the current stage of the ETL
-#        pth = dsPth + '/' + tfNme
-#        pth = Path(pth)
-#        folderExists = False
-#        # Test all stage folders to see if it already exists
-#        for fp in fldPths:
-#            loPth = fp + '/' + tfNme
-#            loPth = Path(loPth)
-#            if loPth.exists():
-#                folderExists = True
-#                print(f'\t\t\tPath: {loPth} - Folder already exists')
-#                break
-#        # If the folder DOES NOT exist then create it.
-#        # if it exist check if it needs to be moved to a different stage
-#        if not folderExists:
-#        # Folder DOES NOT exist so create it
-#            pth.mkdir(exist_ok=True, parents=True)
-#            print('Folder created: ' + str(pth))
-#            return pth
-#        else:
-#            # Folder xists so check if it has been moved to a different stage
-#            if pth != loPth:
-#                print('\tSTAGE has changed for ETL ::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
-#                print('\t\tFrom: ' + str(loPth))
-#                print('\t\tTo:' + str(pth))
-#                shutil.move(loPth, pth)
-#                print('\tETL Moved ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
-
-#            return loPth
-#    except Exception as e:
-#        return 'seeIfFolderExists FAILURE: ' + str(e)
 
 
 
